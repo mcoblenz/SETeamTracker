e5baf08f148bacbc426cbff6e94778304e24a316
Michael Coblenz <mcoblenz@ucsd.edu>
2025-10-09 22:39:47 -0700

    Fixed admin team number.

--- reinitialize.sh
+++ reinitialize.sh
@@ -2,5 +2,5 @@
 
 mv prisma/dev.db archives/$(date +\%Y-\%m-\%d).db
 npx prisma migrate dev --name init
-sqlite3 prisma/dev.db 'insert into User (email, name, team, isAdmin) values("mcoblenz@ucsd.edu", "Michael Coblenz", 0, true);'
+sqlite3 prisma/dev.db 'insert into User (email, name, team, isAdmin) values("mcoblenz@ucsd.edu", "Michael Coblenz", -1, true);'
 
